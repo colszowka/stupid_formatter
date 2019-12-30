c6c32b1bcd43abeb01d95fbe63b0c47e2b7816ce
fliiiix <hi@l33t.name>
2019-12-30 12:14:28 +0100

    Add round bracket for assert_match

--- test/test_erb_formatter.rb
+++ test/test_erb_formatter.rb
@@ -5,29 +5,29 @@ class TestErbFormatter < Test::Unit::TestCase
     setup do
       @text = "<%= 'foo bar baz' %>"
     end
-    
+
     context "after piping it through a new erb formatter instance" do
       setup do
         @result = StupidFormatter::Erb.new(@text).result
       end
-      
+
       should("render 'foo bar baz'") { assert_equal 'foo bar baz', @result.strip.chomp }
     end
   end
-  
+
   context "A chunk of erb-formatted text that includes a block using the highlight-syntax" do
     setup do
       @text = '<% highlight do %>
 puts "foo bar baz"
 <% end %>'
     end
-    
+
     context "after piping it through a new erb instance" do
       setup do
         @result = StupidFormatter::Erb.new(@text).result
       end
-      
-      should('match <div class="CodeRay">') { assert_match /\<div class="CodeRay"\>/, @result.strip.chomp }
+
+      should('match <div class="CodeRay">') { assert_match(/\<div class="CodeRay"\>/, @result.strip.chomp) }
     end
   end
 end
